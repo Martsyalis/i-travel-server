6cb074852a458e56aec79d122763945a8db884e1
MaryusMartsyalis <martsyalis@gmail.com>
2018-01-08 17:44:43 -0800

    Working sockets!

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const app = require('./lib/app');
 const connect = require('./lib/connect');
 
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 const port = process.env.PORT || 3001;
 connect();
+
+io.on('connection', function(socket){
+    console.log('a user connected', socket.id);
+    socket.on('disconnect', function(){
+        console.log('user disconnected', socket.id);
+    });
+    socket.on('chat', function(msg){
+        socket.broadcast.emit('chat', msg);
+        console.log('message: ' + msg);
+    });
+});
 
 server.listen(port, () => {
     // eslint-disable-next-line
